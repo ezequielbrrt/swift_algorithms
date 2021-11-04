f4e057c0bab287d5da14772019407b09783589f2
ezequielbrrt <ezequielbrrt@gmail.com>
2021-11-03 21:29:09 -0600

    Linked list remove nth from parent

--- LinkedList/LinkedList.playground/Contents.swift
+++ LinkedList/LinkedList.playground/Contents.swift
@@ -11,12 +11,13 @@ dogBreeds.append(value: "Husky")
 
 let intLinkedList = LinkedList<Int>()
 intLinkedList.append(value: 1)
+intLinkedList.append(value: 2)
+intLinkedList.append(value: 3)
+//intLinkedList.append(value: 4)
+//intLinkedList.append(value: 5)
 
-print(intLinkedList)
 
 class Solution {
-    
-    var middleToTailElements: [Int] = []
     
     func middleNode(_ head: Node<Int>?) -> Node<Int>? {
         guard let head = head else { return nil }
@@ -28,12 +29,47 @@ class Solution {
             slow = slow?.next
             fast = fast?.next?.next
         }
-        
-        print("The middle element is ")
-        print(slow?.value)
         return slow
+    }
+    
+    func removeNthFromEnd(_ head: Node<Int>?, _ n: Int) -> Node<Int>? {
+        
+        guard let _ = head else { return nil }
+        
+        var auxNode = head
+        var total = 1
+            
+        while auxNode?.next != nil {
+            total += 1
+            auxNode = auxNode?.next
+        }
+        
+        let nthFromEnd = total - n
+        
+        if total == 1 && nthFromEnd == 0 {
+            return nil
+        }
+        
+        if nthFromEnd == 0 {
+            auxNode = head
+            auxNode = head?.next
+            return auxNode
+        }
+        
+        var index = (nthFromEnd == 0) ? 0 : 1
+        var nodeBehindToRemove = head
+        
+        while index != nthFromEnd {
+            index += 1
+            nodeBehindToRemove = nodeBehindToRemove?.next
+        }
+        
+        nodeBehindToRemove?.next = nodeBehindToRemove?.next?.next
+        
+        return head
     }
 }
 
 let solution = Solution()
-solution.middleNode(intLinkedList.fist)
+// solution.middleNode(intLinkedList.fist)
+solution.removeNthFromEnd(intLinkedList.fist, 3)
